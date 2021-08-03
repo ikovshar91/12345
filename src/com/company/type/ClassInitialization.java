d5eda41cdb8017e50e4a16568d8da57c6741fcdc
ikovshar91 <ikovshar91@gmail.com>
2021-08-03 18:35:30 +0600

    14Glava

---
+++ src/com/company/type/ClassInitialization.java
@@ -0,0 +1,42 @@
+package com.company.type;
+
+
+import java.util.Random;
+
+class Initable{
+    static final int staticFinal = 47;
+    static final int staticFinal2 = ClassInitialization.rand.nextInt(1000);
+    static {
+        System.out.println("Инициализация Initable");
+    }
+}
+
+class Initable2{
+    static int staticNonFinal = 147;
+    static {
+        System.out.println("Иниициализация Initable2");
+    }
+}
+
+class Initable3{
+    static int staticNonFinal = 74;
+    static {
+        System.out.println("Инициализация Initable3");
+    }
+}
+
+
+public class ClassInitialization {
+    public static Random rand = new Random(47);
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class initable = Initable.class;
+        System.out.println("После создания ссылки на Initable");
+        System.out.println(Initable.staticFinal);
+        System.out.println(Initable.staticFinal2);
+        System.out.println(Initable2.staticNonFinal);
+        Class initable3 = Class.forName("com.company.type.Initable3");
+        System.out.println("После создания ссылки на Initable3");
+        System.out.println(Initable3.staticNonFinal);
+    }
+}
